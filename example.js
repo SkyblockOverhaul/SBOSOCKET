58daa900943e0a3eb5c08efdc66a8c113b839178
Saotzuri <133770938+Saotzuri@users.noreply.github.com>
2025-03-22 18:25:35 +0100

    Add custom event handling and chat logging functionality to SBOSocket

--- example.js
+++ example.js
@@ -1,14 +1,28 @@
+/* 
+ * this creates a new Event wich needs to be specified in the server
+ */
 socket.addEvent('custom_event');
 
+/* 
+ * this listens for custom events from the server
+ * and processes them
+ */
 socket.on('custom_event', (response) => {
     console.log("[SBO] Verarbeitete Antwort:", JSON.stringify(response, null, 2));
 });
 
+/* 
+ * this connects to the server
+ * and is needed to send and receive events
+ */
 socket.connect();
 
+/* 
+ * this sends a custom event to the server 
+ * and gives the server the ability to respond
+ */
 register("command", (args) => {
     if (args) {
         socket.send('custom_event', args);
     }
-    console.log("[SBO] Verbindung manuell getrennt");
 }).setName("SocketTest");
